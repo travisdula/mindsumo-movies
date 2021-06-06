fbda3c10a5a527442d3b9715faf510e87f11fbc2
Travis A. Dula <travisadula@gmail.com>
2021-06-06 00:29:12 -0500

    fixed search function

--- src/omdb_search.ts
+++ src/omdb_search.ts
@@ -1,12 +1,9 @@
 import axios from "axios";
 
 async function OMDbAPISearch(key: string, title: string): Promise<any> {
-    return axios.get('http://omdbapi.com/', {
-        params: {
-            apikey: key,
-            s: title,
-        }
-    }).then(res => res.data);
+    return axios.get(`http://www.omdbapi.com/?apikey=${key}&s=${title}`)
+    .then(res => {return res.data})
+    .catch(err => console.error(err));
 }
 
 export { OMDbAPISearch };
