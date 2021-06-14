2a44e7d46f8962a80ad20c10eda3e22eff60e7df
Travis A. Dula <travisadula@gmail.com>
2021-06-14 15:47:34 -0500

    improved appearances and removed any type

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { OMDbAPISearch } from './omdb_search';
+import { OMDbAPISearch, OMDbResult } from './omdb_search';
 import { key } from './key.json';
 
 interface AppState {
     query: string;
-    responseArray: Array<any>;
+    responseArray: Array<OMDbResult>;
 }
 
 class App extends React.Component<{}, AppState> {
@@ -18,22 +18,23 @@ class App extends React.Component<{}, AppState> {
 
     render() {
         const movies = this.state.responseArray ? this.state.responseArray
-                .filter(res => res.Type === "movie")
                 .map(res => displayMovie(res))
             : (<div> no results </div>);
         return (
-          <div className="App">
-            <header className="App-header flex justify-center ">
-                <div className="w-3/4 m-4">
+          <div className="App h-screen bg-gray-400">
+            <div className="App-header flex flex-grow justify-center ">
+                <div className="w-3/4 m-6">
+                    {/*
                     <div className="text-center">{"Query: " + this.state.query}</div>
+                    */}
                     <form className="query text-center" onSubmit={this.handleSubmit.bind(this)}>
                         <input onChange={this.handleChange.bind(this)} placeholder="search a movie" />
                     </form>
-                    <div className="w-auto">
-                        { movies }
-                    </div>
                 </div>
-            </header>
+            </div>
+           <div className="w-auto">
+               { movies }
+           </div>
           </div>
         );
     }
@@ -48,19 +49,20 @@ class App extends React.Component<{}, AppState> {
     async handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const response = await OMDbAPISearch(key, this.state.query)
-        console.log(response.Search);
+        console.log(response);
         this.setState({
             query: this.state.query,
-            responseArray: response.Search
+            responseArray: response.Search,
         });
     }
 }
 
-function displayMovie(result: any) {
+function displayMovie(result: OMDbResult) {
     return (
-        <div key={result.Title + result.Year} className="movie flex flex-row">
-            <span className="title flex-grow"> {result.Title} </span>
-            <span className="year text-right flex-grow"> {result.Year} </span>
+        <div key={result.Title + result.Year} className="movie bg-gray-700 m-2 flex flex-row">
+            <img className="w-32" src={result.Poster} alt="movie poster" />
+            <span className="title text-lg text-gray-50 font-semibold flex-grow"> {result.Title} </span>
+            <span className="year text-gray-100 text-right flex-grow"> {result.Year} </span>
         </div>
     );
 }
