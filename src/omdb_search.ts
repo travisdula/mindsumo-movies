2cf4c4e5b9e60c549e56053999c7c6936816f13b
Travis A. Dula <travisadula@gmail.com>
2021-06-14 15:47:06 -0500

    added interfaces for API types

--- src/omdb_search.ts
+++ src/omdb_search.ts
@@ -1,6 +1,20 @@
 import axios from "axios";
 
-async function OMDbAPISearch(key: string, title: string): Promise<any> {
+export interface OMDbResponse {
+    Search: Array<OMDbResult>;
+    totalResults: string;
+    Response: string;
+}
+
+export interface OMDbResult {
+    Title: string;
+    Year: string;
+    imdbID: string;
+    Poster: string;
+    Type: string;
+
+}
+async function OMDbAPISearch(key: string, title: string): Promise<OMDbResponse> {
     return axios.get(`https://www.omdbapi.com/?apikey=${key}&s=${title}`)
     .then(res => {return res.data})
     .catch(err => console.error(err));
