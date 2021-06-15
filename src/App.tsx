6a4e1c2f8718543021c14e279af604b47292328e
Travis A. Dula <travisadula@gmail.com>
2021-06-15 09:00:25 -0500

    moved Movie component into separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { OMDbAPISearch, OMDbSearchResult, OMDbAPIGetByID, DetailedOMDbResponse } from './omdb_search';
+import { OMDbAPISearch, OMDbSearchResult } from './omdb_search';
 import { key } from './key.json';
+import { Movie } from './Movie';
 
 interface AppState {
     query: string;
@@ -34,7 +35,7 @@ class App extends React.Component<{}, AppState> {
             : null;
         const movies = this.state.responseArray ?
             this.state.responseArray
-            .map(res => (<Movie value={res}/>))
+            .map(res => (<Movie result={res}/>))
             : (<div> no results </div>);
         const button = this.state.responseArray.length === this.state.totalResults ?
             null
@@ -107,37 +108,7 @@ class App extends React.Component<{}, AppState> {
 }
 
 
-function Movie({ value }: any) {
-    const result = value;
-    console.log("res", result);
-    const [expanded, setExpanded] = React.useState(false);
-    const [moreInfo, setMoreInfo]
-        : [DetailedOMDbResponse | undefined, React.Dispatch<DetailedOMDbResponse | undefined>]
-        = React.useState();
-    return (
-        <button 
-            key={result.imdbID}
-            onClick={async () =>
-                {
-                    !(moreInfo && 'Title' in moreInfo) &&
-                        setMoreInfo(await OMDbAPIGetByID(key, result.imdbID));
-                    setExpanded(!expanded);
-                }
-            }
-            className="movie transition duration-500 rounded-lg bg-opacity-25 hover:bg-opacity-100 md:w-96 w-11/12 bg-gray-400 m-4 mt-0 ">
-            <div className="flex flew-row flex-auto">
-                <img className="w-32 flex-shrink-0 rounded-lg text-gray-500 italic text-center" src={result.Poster} alt="no poster" />
-                <div className="flex flex-col w-full m-2">
-                    <div className="flex flex-row">
-                        <span className="title text-lg text-gray-700 text-left font-semibold flex-grow"> {result.Title} </span>
-                        <span className="year text-gray-600 text-right flex-grow"> {result.Year} </span>
-                    </div>
-                    { expanded && (<div> {moreInfo && moreInfo.Plot} </div>) }
-                </div>
-            </div>
-        </button>
-    );
-}
+
 
 
 export default App;
---
+++ src/Movie.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { DetailedOMDbResponse, OMDbAPIGetByID } from './omdb_search'
+import { key  }from './key.json';
+
+export function Movie({ result }: any) {
+    console.log("res", result);
+    const [expanded, setExpanded] = React.useState(false);
+    const [moreInfo, setMoreInfo]
+        : [DetailedOMDbResponse | undefined, React.Dispatch<DetailedOMDbResponse | undefined>]
+        = React.useState();
+    return (
+        <button 
+            key={result.imdbID}
+            onClick={async () =>
+                {
+                    !(moreInfo && 'Title' in moreInfo) &&
+                        setMoreInfo(await OMDbAPIGetByID(key, result.imdbID));
+                    setExpanded(!expanded);
+                }
+            }
+            className="movie transition duration-500 rounded-lg bg-opacity-25 hover:bg-opacity-100 md:w-96 w-11/12 bg-gray-400 m-4 mt-0 ">
+            <div className="flex flew-row flex-auto">
+                <img className="w-32 flex-shrink-0 rounded-lg text-gray-500 italic text-center" src={result.Poster} alt="no poster" />
+                <div className="flex flex-col w-full m-2">
+                    <div className="flex flex-row">
+                        <span className="title text-lg text-gray-700 text-left font-semibold flex-grow"> {result.Title} </span>
+                        <span className="year text-gray-600 text-right flex-grow"> {result.Year} </span>
+                    </div>
+                    { expanded && (<div className="text-sm"> {moreInfo && moreInfo.Plot} </div>) }
+                </div>
+            </div>
+        </button>
+    );
+}
