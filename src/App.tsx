d8246a60ba94470e6e40dcd7cf26eb65d72cac76
Travis A. Dula <travisadula@gmail.com>
2021-06-15 08:54:00 -0500

    improved types, changed displayMovie to function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { OMDbAPISearch, OMDbResult } from './omdb_search';
+import { OMDbAPISearch, OMDbSearchResult, OMDbAPIGetByID, DetailedOMDbResponse } from './omdb_search';
 import { key } from './key.json';
 
 interface AppState {
     query: string;
-    responseArray: Array<OMDbResult>;
+    responseArray: Array<OMDbSearchResult>;
     index: number;
     totalResults: number;
     error: string;
@@ -34,7 +34,7 @@ class App extends React.Component<{}, AppState> {
             : null;
         const movies = this.state.responseArray ?
             this.state.responseArray
-            .map(res => displayMovie(res))
+            .map(res => (<Movie value={res}/>))
             : (<div> no results </div>);
         const button = this.state.responseArray.length === this.state.totalResults ?
             null
@@ -106,14 +106,35 @@ class App extends React.Component<{}, AppState> {
     }
 }
 
-function displayMovie(result: OMDbResult) {
+
+function Movie({ value }: any) {
+    const result = value;
+    console.log("res", result);
+    const [expanded, setExpanded] = React.useState(false);
+    const [moreInfo, setMoreInfo]
+        : [DetailedOMDbResponse | undefined, React.Dispatch<DetailedOMDbResponse | undefined>]
+        = React.useState();
     return (
         <button 
             key={result.imdbID}
-            className="movie transition duration-500 rounded-lg bg-opacity-25 hover:bg-opacity-100 md:w-96 w-11/12 bg-gray-400 m-4 mt-0 flex flex-row">
-            <img className="w-32 flex-shrink-0 rounded-lg text-gray-500 italic text-center" src={result.Poster} alt="no poster" />
-            <span className="title m-2 text-lg text-gray-700 font-semibold"> {result.Title} </span>
-            <span className="year m-2 text-gray-600 text-right flex-grow"> {result.Year} </span>
+            onClick={async () =>
+                {
+                    !(moreInfo && 'Title' in moreInfo) &&
+                        setMoreInfo(await OMDbAPIGetByID(key, result.imdbID));
+                    setExpanded(!expanded);
+                }
+            }
+            className="movie transition duration-500 rounded-lg bg-opacity-25 hover:bg-opacity-100 md:w-96 w-11/12 bg-gray-400 m-4 mt-0 ">
+            <div className="flex flew-row flex-auto">
+                <img className="w-32 flex-shrink-0 rounded-lg text-gray-500 italic text-center" src={result.Poster} alt="no poster" />
+                <div className="flex flex-col w-full m-2">
+                    <div className="flex flex-row">
+                        <span className="title text-lg text-gray-700 text-left font-semibold flex-grow"> {result.Title} </span>
+                        <span className="year text-gray-600 text-right flex-grow"> {result.Year} </span>
+                    </div>
+                    { expanded && (<div> {moreInfo && moreInfo.Plot} </div>) }
+                </div>
+            </div>
         </button>
     );
 }
